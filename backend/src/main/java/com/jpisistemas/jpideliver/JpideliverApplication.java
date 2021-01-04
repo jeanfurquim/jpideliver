00d0cec0dbc1bbb18954b7fef92570c4f9a699e8
jeanfurquim <jean.furquim@jpisistemas.com.br>
2021-01-04 12:01:04 -0300

    Project created

---
+++ backend/src/main/java/com/jpisistemas/jpideliver/JpideliverApplication.java
@@ -0,0 +1,13 @@
+package com.jpisistemas.jpideliver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpideliverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpideliverApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/jpisistemas/jpideliver/JpideliverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jpisistemas.jpideliver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpideliverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
