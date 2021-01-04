b82981a61f2cb20fcf5bb16a33c7b25a694e615d
jeanfurquim <jean.furquim@jpisistemas.com.br>
2021-01-04 16:45:29 -0300

    GET /orders

---
+++ backend/src/main/java/com/jpisistemas/jpideliver/controllers/OrderController.java
@@ -0,0 +1,30 @@
+package com.jpisistemas.jpideliver.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.jpisistemas.jpideliver.dto.OrderDto;
+import com.jpisistemas.jpideliver.services.OrderService;
+
+@RestController
+@RequestMapping(value = "/orders")
+public class OrderController {
+	
+	@Autowired
+	private OrderService service;
+	
+	@GetMapping
+		public ResponseEntity<List<OrderDto>> findAll(){
+		List<OrderDto> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+		
+	}
+	
+	
+
+}
