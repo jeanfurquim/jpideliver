cbad7509f5349e554212f719302461534dde1d94
jeanfurquim <jean.furquim@jpisistemas.com.br>
2021-01-06 15:44:52 -0300

    Add Routes and StepsHeader

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,14 +1,10 @@
 import './App.css';
-import Home from './Home/Index';
-import Navbar from './Navbar';
+
+import Routes from './Routes';
 
 function App() {
   return (
-    <>
-  <Navbar />
-  <Home />
-
-    </>
+  <Routes />
   );
 }
 
